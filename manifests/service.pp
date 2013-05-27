f56fa60896a9f033b4a3e8f48cf9126da6f56b1a
Tom De Vylder <tom@penumbra.be>
2013-05-27 13:35:14 +0200

    Add missing parameter: $service_hasrestart

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,11 @@
 #
 class ftpproxy::service {
   service { $::ftpproxy::service_name:
-    ensure    => $::ftpproxy::service_ensure,
-    enable    => $::ftpproxy::service_enable,
-    hasstatus => $::ftpproxy::service_hasstatus,
-    require   => Class['::ftpproxy::config'];
+    ensure     => $::ftpproxy::service_ensure,
+    enable     => $::ftpproxy::service_enable,
+    hasrestart => $::ftpproxy::service_hasrestart,
+    hasstatus  => $::ftpproxy::service_hasstatus,
+    require    => Class['::ftpproxy::config'];
   }
 }
 
